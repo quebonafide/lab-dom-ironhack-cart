94b7cc33a8428144f3126af9369994c60652e4e5
Michael Heimrath <mheimrath1994@gmail.com>
2021-01-19 15:18:53 +0100

    almost done

--- js/index.js
+++ js/index.js
@@ -2,41 +2,64 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
+  const priceSelector = product.querySelector('.price span');
+  const quantitySelector = product.querySelector('.quantity input[value]');
+  const subtotalSelector = product.querySelector('.subtotal span');
+  const price = priceSelector.innerText;
+  const quantity = quantitySelector.value;
+  const subtotal = price * quantity;
+  subtotalSelector.textContent = subtotal;
+  return subtotal;
   //... your code goes here
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+ singleProduct = document.querySelectorAll('.product');
+ let total = 0;
+ for (element of singleProduct) {
+  total += updateSubtotal(element);
+ }
+totalAmount = document.querySelector('#total-value span');
+totalAmount.textContent = total;
 }
 
 // ITERATION 4
-
 function removeProduct(event) {
   const target = event.currentTarget;
+  target.parentNode.parentNode.remove();
   console.log('The target in remove is:', target);
+  calculateAll();
+
   //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
+  const nameInput = document.querySelector('#cart > tfoot > tr > td:nth-child(1) > input[type=text]').innerText;
+  const priceInput = document.querySelector('#cart > tfoot > tr > td:nth-child(2) > input[type=number]').value;
   //... your code goes here
+  const templet = document.getElementById('hidden-template')
+  console.log(nameInput);
+  console.log(priceInput);
+  const product = document.querySelector('.product');
+  const clone = product.cloneNode(true);
+  document.getElementById('tbody').appendChild(clone);
 }
+const template = document.getElementById('hidden-template')
+console.log(template);
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
   //... your code goes here
+  
+   const removeElement = document.getElementsByClassName("btn btn-remove");
+  for(i = 0; i < removeElement.length; i++) {
+    removeElement[i].addEventListener('click', removeProduct);
+  }
+  const addElement = document.getElementById('create');
+  addElement.addEventListener('click', createProduct);
+
 });
